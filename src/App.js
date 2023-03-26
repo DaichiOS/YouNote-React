caba773d5cd710100e4028e49ea5fc8fff3a5430
FRESHVIEW\eddie.zeng <eddie.zeng@campaignmonitor.com>
2023-03-26 17:37:14 +1100

    added working sockets, live streaming of gpt data and broken minimise/maximise functionality

--- src/App.js
+++ src/App.js
@@ -1,28 +1,121 @@
 import React, { useEffect } from "react";
 import { io } from "socket.io-client";
+import "./App.css";
+import hideIcon from "./assets/hide-icon.svg";
+import showIcon from "./assets/show-icon.svg";
 
 const SOCKET_SERVER_URL = "http://127.0.0.1:8001";
+const SUMMARY_STORAGE_KEY = "video_summary";
 
 function App() {
-  useEffect(() => {
-    const socket = io(SOCKET_SERVER_URL);
+  const socketRef = React.useRef();
+  const [videoId, setVideoId] = React.useState("");
+  const [summary, setSummary] = React.useState(() => {
+    return localStorage.getItem(SUMMARY_STORAGE_KEY) || "";
+  });
 
-    socket.on("connect", () => {
-      console.log("Connected to server");
+  useEffect(() => {
+    if (!videoId) {
+      return;
+    }
+
+    setSummary(""); // Reset the summary state when a new video ID is set
+
+    socketRef.current = io(SOCKET_SERVER_URL);
+    socketRef.current.on("connect", () => {
+      socketRef.current.emit("processVideoId", videoId);
     });
 
-    socket.on("message", (message) => {
-      console.log("Message from server:", message);
+    socketRef.current.on("chatgpt_response", (newText) => {
+      setSummary((prevSummary) => prevSummary + newText);
     });
 
     return () => {
-      socket.disconnect();
+      socketRef.current.disconnect();
     };
+  }, [videoId]);
+
+  useEffect(() => {
+    localStorage.setItem(SUMMARY_STORAGE_KEY, summary);
+  }, [summary]);
+
+  const handleSummariseVideoClick = () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const tabId = tabs[0].id;
+
+      chrome.tabs.get(tabId, (tab) => {
+        const url = tab.url;
+        if (!url) {
+          console.log("Error: URL is empty or undefined");
+          return;
+        }
+
+        try {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          const id = urlParams.get("v");
+          if (id) {
+            setVideoId(id);
+            if (socketRef.current) {
+              socketRef.current.emit("processVideoId", id);
+            }
+          } else {
+            console.log("Error: Video ID not found in URL");
+          }
+        } catch (error) {
+          console.log("Error: Failed to parse URL", error);
+        }
+      });
+    });
+  };
+
+  useEffect(() => {
+    const button = document.getElementById("summarise-button");
+    if (button) {
+      button.addEventListener("click", handleSummariseVideoClick);
+
+      return () => {
+        button.removeEventListener("click", handleSummariseVideoClick);
+      };
+    }
   }, []);
 
+  const [isSummaryMinimized, setIsSummaryMinimized] = React.useState(false);
+
+  const handleToggleSummary = () => {
+    if (isMinimized) {
+      setMinimized(false);
+      setToggleIconSrc(hideIcon);
+    } else {
+      setMinimized(true);
+      setToggleIconSrc(showIcon);
+    }
+  };
+
+  const summaryClass = isSummaryMinimized ? "minimized" : "";
+
   return (
-    <div className="App">
-      <h1>Test</h1>
+    <div className={`App ${summaryClass}`}>
+      <button id="summarise-button" onClick={handleSummariseVideoClick}>
+        Summarise Video
+      </button>
+      <div className="summary-container">
+        {isSummaryMinimized ? (
+          <img
+            src={showIcon}
+            alt="Expand summary"
+            className="toggle-icon"
+            onClick={handleToggleSummary}
+          />
+        ) : (
+          <img
+            src={hideIcon}
+            alt="Minimize summary"
+            className="toggle-icon"
+            onClick={handleToggleSummary}
+          />
+        )}
+        <div className="summary-text">{summary}</div>
+      </div>
     </div>
   );
 }
