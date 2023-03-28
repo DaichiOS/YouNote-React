d5a3d25f9379ba538549640e2c417ccce86853c4
FRESHVIEW\eddie.zeng <eddie.zeng@campaignmonitor.com>
2023-03-29 00:41:55 +1100

    revert

--- src/background.js
+++
@@ -1,7 +0,0 @@
-// background.js
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.type === "PROCESS_VIDEO_ID") {
-    // Move the socket connection and summary generation code here
-  }
-});
